e07fc6ffe25cac7158826e198d9df63fc76d7a36
Brett <student@epicodus.com>
2015-04-24 09:23:15 -0700

    add Student Controller

---
+++ controllers/StudentsController.js
@@ -0,0 +1,13 @@
+signIn.controller('StudentsCtrl', function StudentsCtrl($scope) {
+    $scope.students = [
+        { name: "Smicky McCoodle", signedIn: false},
+        { name: "Ted Nugent ", signedIn: false},
+        { name: "Francis Bacon", signedIn: false },
+        { name: "Hans Olo",signedIn: false },
+        { name: "Sharon Peters", signedIn: false },
+        { name: "Sharon Needles", signedIn: false },
+        { name: "Larry Kryzinsky", signedIn: false },
+        { name: "Greg Alohia ", signedIn: false },
+        { name: "Tim Tilliwink", signedIn: false },
+        { name: "Woodsley Hardington", signedIn: false }
+    ];
