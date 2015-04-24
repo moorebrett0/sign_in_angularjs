cc9bc12fc79e2918bf4951ec39ed0300f59129f5
Brett <student@epicodus.com>
2015-04-24 09:13:20 -0700

    add scripts to index.html

---
+++ app.js
@@ -0,0 +1,8 @@
+  var signIn = angular.module("signIn", ["ui.router"]);
+
+  signIn.config(function($stateProvider) {
+      $stateProvider.state("home", {
+          url: "",
+          templateUrl: "partials/home.html"
+      });
+  });
